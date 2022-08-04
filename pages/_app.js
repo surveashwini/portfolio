d4286e5b82ba844a84d7caa1c586b31ca2dcaa5b
Ashwini Surve <ashwini.mca.89@gmail.com>
2022-08-04 18:24:18 -0400

    upgrade portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import "../styles/global.css";
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Ashwini Surve | Full-stack Developer</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
